3152da1f843707474d67897fda02f7a83684bb9c
David Perez <david@closemarketing.es>
2017-02-08 12:43:02 +0100

    limpiado header

--- includes/one-category.php
+++ includes/one-category.php
@@ -1,9 +1,4 @@
 <?php
-/*
-Plugin name: Una sola categoría
-Version: 1.0
-Description: Reemplaza la casilla de selección de categorías del editor por botones radio para que solo puedas elegir una categoría por entrada.
-*/
 function cmk_admin_catcher() {
 	if( strstr($_SERVER['REQUEST_URI'], 'wp-admin/post-new.php') 
 		|| strstr($_SERVER['REQUEST_URI'], 'wp-admin/post.php') 
