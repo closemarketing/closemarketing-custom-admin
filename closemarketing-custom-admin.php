05cadd501cb74bd4f2e97e580b28f35a0cf5ce77
David Perez <david@closemarketing.es>
2017-06-06 22:56:17 +0200

    optimization

--- closemarketing-custom-admin.php
+++ closemarketing-custom-admin.php
@@ -15,17 +15,11 @@
 //Loads translation
 load_plugin_textdomain('closemarketing-custom-admin', false, dirname( plugin_basename( __FILE__ ) ). '/languages/');
 
-require __DIR__ . '/vendor/autoload.php';
-
 define( 'CMK_RESIZE_WIDTH', 1920 ); //1000 px wide
 define( 'CMK_RESIZE_HEIGHT', 1920 ); //900 px high
 
 define( 'CMK_RESIZE_QUALITY', 70 ); //0-100, 100 being high quality
 define( 'CMK_MAX_UPLOAD_SIZE', '10971520b' ); //size in bytes
 
-//* Includes General for Closemarketing
-include('includes/clean-url-seo.php'); //cleans stop words from slug
-include('includes/one-category.php'); //only allows to select one category_archive_meta
-include('includes/wp-admin-default.php'); // Custom Closemarketing Dashboard
-include('includes/wp-plugins-recommended.php'); //Recommends plugins
-include('includes/resize-before-upload.php'); //Resize big images to normal size
+//* Includes Libraries for Closemarketing
+foreach ( glob( dirname( __FILE__ ) . '/includes/*.php' ) as $file ) { include_once $file; }
