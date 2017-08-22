df53ba25f145653e3d754d96c33178fcd727bd1f
David Perez <david@closemarketing.es>
2017-08-22 10:45:46 +0200

    static call to php files

--- closemarketing-custom-admin.php
+++ closemarketing-custom-admin.php
@@ -13,13 +13,24 @@
  */
 
 //Loads translation
-load_plugin_textdomain('closemarketing-custom-admin', false, dirname( plugin_basename( __FILE__ ) ). '/languages/');
+load_plugin_textdomain('closemarketing-custom-admin', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 
-define( 'CMK_RESIZE_WIDTH', 1920 ); //1000 px wide
-define( 'CMK_RESIZE_HEIGHT', 1920 ); //900 px high
+define('CMK_RESIZE_WIDTH', 1920); //1000 px wide
+define('CMK_RESIZE_HEIGHT', 1920); //900 px high
 
-define( 'CMK_RESIZE_QUALITY', 70 ); //0-100, 100 being high quality
-define( 'CMK_MAX_UPLOAD_SIZE', '10971520b' ); //size in bytes
+define('CMK_RESIZE_QUALITY', 70); //0-100, 100 being high quality
+define('CMK_MAX_UPLOAD_SIZE', '10971520b'); //size in bytes
 
 //* Includes Libraries for Closemarketing
-foreach ( glob( dirname( __FILE__ ) . '/includes/*.php' ) as $file ) { include_once $file; }
+include_once dirname(__FILE__) . '/includes/one-category.php';
+
+include_once dirname(__FILE__) . '/includes/clean-url-seo.php';
+
+include_once dirname(__FILE__) . '/includes/resize-before-upload.php';
+
+include_once dirname(__FILE__) . '/includes/wp-admin-default.php';
+
+//* Plugins recommended
+include_once dirname(__FILE__) . '/includes/class-tgm-plugin-activation.php';
+
+include_once dirname(__FILE__) . '/includes/wp-plugins-recommended.php';
