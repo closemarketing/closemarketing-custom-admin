9d59968a37551389da2d930afa29a88094dff9b7
David Perez <david@closemarketing.es>
2018-07-24 16:52:35 +0200

    phpcs WordPress coding standars

--- closemarketing-custom-admin.php
+++ closemarketing-custom-admin.php
@@ -10,27 +10,29 @@
  * Domain Path: /languages
  * License: GNU General Public License version 3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package WordPress
  */
 
-//Loads translation
-load_plugin_textdomain('closemarketing-custom-admin', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+// * Loads translation
+load_plugin_textdomain( 'closemarketing-custom-admin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-define('CMK_RESIZE_WIDTH', 1920); //1000 px wide
-define('CMK_RESIZE_HEIGHT', 1920); //900 px high
+define( 'CMK_RESIZE_WIDTH', 1920 ); // 1000 px wide
+define( 'CMK_RESIZE_HEIGHT', 1920 ); // 900 px high
 
-define('CMK_RESIZE_QUALITY', 70); //0-100, 100 being high quality
-define('CMK_MAX_UPLOAD_SIZE', '10971520b'); //size in bytes
+define( 'CMK_RESIZE_QUALITY', 70 );
+define( 'CMK_MAX_UPLOAD_SIZE', '10971520b' );
 
-//* Includes Libraries for Closemarketing
-include_once dirname(__FILE__) . '/includes/one-category.php';
+// * Includes Libraries for Closemarketing
+require_once dirname( __FILE__ ) . '/includes/one-category.php';
 
-include_once dirname(__FILE__) . '/includes/clean-url-seo.php';
+require_once dirname( __FILE__ ) . '/includes/clean-url-seo.php';
 
-include_once dirname(__FILE__) . '/includes/resize-before-upload.php';
+require_once dirname( __FILE__ ) . '/includes/resize-before-upload.php';
 
-include_once dirname(__FILE__) . '/includes/wp-admin-default.php';
+require_once dirname( __FILE__ ) . '/includes/wp-admin-default.php';
 
-//* Plugins recommended
-include_once dirname(__FILE__) . '/includes/class-tgm-plugin-activation.php';
+// * Plugins recommended
+require_once dirname( __FILE__ ) . '/includes/class-tgm-plugin-activation.php';
 
-include_once dirname(__FILE__) . '/includes/wp-plugins-recommended.php';
+require_once dirname( __FILE__ ) . '/includes/wp-plugins-recommended.php';
