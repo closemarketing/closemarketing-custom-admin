e8ef9e7c022371057f7962fd43d295eee39efd58
davidperezgar <david@closemarketing.es>
2023-08-07 13:41:24 +0200

    updated background login

--- closemarketing-custom-admin.php
+++ closemarketing-custom-admin.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name: Closemarketing Custom Admin
+ * Plugin Name: CLOSE Custom Admin
  * Plugin URI: https://close.marketing
- * Description: Enhacements WordPress admin for Closemarketing.
+ * Description: Enhacements WordPress admin for CLOSE webs.
  * Author: closemarketing
  * Author URI: https://close.marketing/
- * Version: 1.11
+ * Version: 1.12.0
  * Text Domain: closemarketing-custom-admin
  * Domain Path: /languages
  * License: GNU General Public License version 3.0
@@ -14,20 +14,25 @@
  * @package WordPress
  */
 
-// * Loads translation
+define( 'CLOSEAD_VERSION', '1.12.0' );
+define( 'CLOSEAD_PLUGIN', __FILE__ );
+define( 'CLOSEAD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'CLOSEAD_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+// Loads translation.
 load_plugin_textdomain( 'closemarketing-custom-admin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 
-// * Includes Libraries for Closemarketing
-require_once dirname( __FILE__ ) . '/includes/class-cca-wpadmin.php';
+// Includes Libraries for Closemarketing.
+require_once CLOSEAD_PLUGIN_PATH . 'includes/class-cca-wpadmin.php';
+require_once CLOSEAD_PLUGIN_PATH . 'includes/class-cca-optimizer.php';
 
-// * Plugins recommended
-require_once dirname( __FILE__ ) . '/includes/class-tgm-plugin-activation.php';
-
-require_once dirname( __FILE__ ) . '/includes/wp-plugins-recommended.php';
+// Plugins recommended.
+require_once CLOSEAD_PLUGIN_PATH . 'includes/class-tgm-plugin-activation.php';
+require_once CLOSEAD_PLUGIN_PATH . 'includes/wp-plugins-recommended.php';
 
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	require_once dirname( __FILE__ ) . '/includes/woocommerce.php';
+	require_once CLOSEAD_PLUGIN_PATH . 'includes/woocommerce.php';
 }
 
 // Find Genesis Theme Data.
@@ -35,6 +40,9 @@ $theme = wp_get_theme( 'genesis' );
 
 // Restrict activation to only when the Genesis Framework is activated.
 if ( basename( get_template_directory() ) === 'genesis' ) {
-	require_once dirname( __FILE__ ) . '/includes/genesis.php';
+	require_once CLOSEAD_PLUGIN_PATH . 'includes/genesis.php';
 }
 
+
+new CCA_WPAdmin();
+new CCA_Optimizer();
